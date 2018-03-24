dddb7653971e873599ecae7faac3186ecc47f40d
Angelina Li <ali6@wellesley.edu>
2018-03-24 13:58:01 -0400

    updated form objects

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,13 @@
 from flask_wtf import FlaskForm
 
+class LoginForm(FlaskForm):
+    pass
+
+class SignUpForm(FlaskForm):
+    pass
+
+class SettingsForm(FlaskForm):
+    pass
+
+class NewEventForm(FlaskForm):
+    pass
