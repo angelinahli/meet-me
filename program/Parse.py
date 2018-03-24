87346fc5d6c12ce5226b8209f6fa3cf29b2a112e
Sarah Seidman <ss5311@barnard.edu>
2018-03-24 12:48:35 -0400

    additions

--- program/Parse.py
+++ program/Parse.py
@@ -1,19 +1,31 @@
-from icalendar import Calendar, Event #no module named icalendar?
+from icalendar import Calendar, Event
 from datetime import datetime
 from pytz import UTC # timezone
 
-person = User()
+#person = User()
 
 c = open("test_files/calendar.ics", "rb")
 cal = Calendar.from_ical(c.read())
-for component in cal.walk():
-    if component.name == "VEVENT":
-        name = component.get('summary')
-        start = component.get('dtstart')
-        end = component.get('dtend')
-        temp = Event(name, start, end)
+for component in cal.walk("vevent"):
+    name = component.get('summary')
+    start = component.get('dtstart')
+    end = component.get('dtend')
+    #temp = Event(name, start, end)
+    print name
+    print start
+    print end
 
-        # add temp event to user's array of events
+    # add temp event to user's array of events
 
         
-g.close()
+c.close()
+
+# import vobject
+
+# data = open("test_files/calendar.ics").read()
+
+# cal = vobject.readOne(data)
+
+# for component in cal.walk():
+# 	print cal.vevent.summary.valueRepr()
+
