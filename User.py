30c3b5bd202337e8169ac89fd4b400f6794d13d7
Nisha Choudhary <nchoudhary@brynmawr.edu>
2018-03-24 12:48:00 -0400

    User
    User object

---
+++ User.py
@@ -0,0 +1,25 @@
+"""
+This class is meant to store the user information like the user's name,
+username, email, and events so that it could be used to compare schedules and
+find free time with other users
+"""
+
+name = "User"
+username = ""
+email = ""
+events = []
+
+class User(object):
+    def __init__(self, name, username, email):
+        if(type(name) == type("")):
+            self.name = name
+
+        if(type(username) == type("")):
+            self.username = username
+        self.email = email
+
+    def user_events(userEvents):
+        if(type(userEvents) == type([0, 1, 2, 3, 4])):
+            events = userEvents
+        else:
+            events = []
