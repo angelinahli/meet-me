c243d2e9cbcb519f900b85ae1d5876ae0b1b853a
Sarah Seidman <ss5311@barnard.edu>
2018-03-24 14:02:38 -0400

    added list

--- program/Parse.py
+++ program/Parse.py
@@ -4,18 +4,22 @@ from pytz import UTC # timezone
 from Event import Event
 
 
-class Parse:
+def parseCal(link): #this is a static method?
 
-	def parseCal(link): #this is a static method?
-	
-		c = open("link", "rb")
-		cal = Calendar.from_ical(c.read())
-		for component in cal.walk("vevent"):
-		    name = component.get('summary')
-		    start = component.decoded('dtstart')
-		    end = component.decoded('dtend')
-		    temp = Event(name, start, end)
-		    #add temp event to user's list of events
-		        
-		c.close()
+	list = []
+
+	c = open(link, "rb")
+	cal = Calendar.from_ical(c.read())
+	for component in cal.walk("vevent"):
+	    name = component.get('summary')
+	    start = component.decoded('dtstart')
+	    end = component.decoded('dtend')
+	    temp = Event(name, start, end)
+	    
+	    list.append(temp)
+	        
+	c.close()
+
+
+
 
