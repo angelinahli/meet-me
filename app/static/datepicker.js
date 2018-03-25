c56314ff91c5ed6e48f888e39f1e948a09f40297
Sarah Seidman <ss5311@barnard.edu>
2018-03-25 09:20:26 -0400

    changes

--- app/static/datepicker.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#start").datepicker();
-  });
-
-
- var startdate = $("#start").datepicker("getDate()");
- console.log(startdate);
---
+++ app/static/picking.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	console.log("working");
+	debugger;
+    // $("#start").datepicker();
+  });
+
+
+ // var startdate = $("#start").datepicker("getDate()");
+ // console.log(startdate);
