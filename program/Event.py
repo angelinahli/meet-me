6ce2d71c32429be4b7b61f9508d1f80c66cfa15f
Angelina Li <ali6@wellesley.edu>
2018-03-24 23:36:12 -0400

    cleaned up files

--- program/Event.py
+++ program/Event.py
@@ -6,17 +6,8 @@ class Event(object):
 		self.name = name
 		self.start = start #start and end are datetime objects
 		self.end = end
-
-	def getLength(self):
-		length = self.end-self.start
-		return length #this should be timedelta
-
-	def getStart(self):
-		return self.start
-
-	def getEnd(self):
-		return self.end
-
+		self.length = self.end - self.start
+		
 	def __str__(self):
 		out = strftime(start) + " to " + strftime(end)
 		return out
--- program/Parse.py
+++ program/Parse.py
@@ -4,89 +4,45 @@ from pytz import UTC # timezone
 from Event import Event
 from datetime import datetime, timedelta
 
-
 def parseCal(link):
-
-	list = []
-
-	c = open(link, "rb")
-	cal = Calendar.from_ical(c.read())
+	"""Returns a list of all events in someone's calendar"""
+	lst = []
+	with open(link, "rb") as c:
+		all_text = c.read()
+	
+	cal = Calendar.from_ical(all_text)
 	for component in cal.walk("vevent"):
 	    name = component.get('summary')
 	    start = component.decoded('dtstart')
 	    end = component.decoded('dtend')
 	    temp = Event(name, start, end)
-	    
-	    list.append(temp)
-	        
-	c.close()
+	    lst.append(temp)
+	return lst
 
-	return list
+def isConflicting(evt1, evt2):
+	return (evt1.start < evt2.end) and (evt1.end > evt2.start)
 
 def scheduler(user1, user2, length):
-
-	if(user1.getStart()>user2.getStart()):
-		start = user1.getStart()
-	else:
-		start = user2.getStart()
-
-	if(user1.getEnd()<user2.getEnd()):
-		end = user2.getEnd()
-	else:
-		end = user1.getEnd()
-
+	start = max(user1.start, user2.start)
+	end = min(user1.end, user2.end)
 	leng = timedelta(minutes=length)
 
 	pos = []
-	i=start
-	while(i<end-leng): #populates list with all possible appointments
-		temp = Event("free", i, (i+leng))
-		pos.append(temp)
-		i = i+leng
+	time = start
+	while(time < end - leng):
+		new_event = Event("free", time, time + leng)
+		pos.append(new_event)
+		i += leng
 
-	sched = [] #empty list, will be filled with times that work
+	sched = [] # empty list, will be filled with times that work
+	conflicts = user1.events + user2.events
 
-	u1 = user1.getEvents()
-	u2 = user2.getEvents()
-	conflicts = u1 + u2
-
-	for element in pos:
-		conflicts = False
-		for Event in conflicts:
-			if isConflicting(element, evt):
-				conflicts = True
+	for candidate in pos:
+		has_conflict = False
+		for event in conflicts:
+			if isConflicting(candidate, event):
+				has_conflict = True
 				break
-		if not conflicts:
+		if not has_conflict:
 			sched.append(element)
-
-
 	return sched
-
-
-
-def isConflicting(evt1, evt2):
-	conflicting = False
-
-	if(evt1.getStart() < evt2.getEnd() and evt1.getEnd() > evt2.getStart()): #not 100% confident in logic
-		conflicting = True
-
-	return conflicting
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
